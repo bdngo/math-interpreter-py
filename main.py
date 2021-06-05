d065d1e59f3293bed86164df151ccc716d772c13
Bryan Ngo <bryanngo@berkeley.edu>
2021-06-05 13:25:18 -0700

    split infix and postfix shells

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
-from interpreter import Interpreter
+import sys
+
+from interpreter import Interpreter, postfix_eval
 from parser_ import Parser
 from lexer import Lexer
 
-while True:
-    try:
-        txt = input("calc > ")
-        lexer = Lexer(txt)
-        tokens = lexer.generate_tokens()
 
-        parser = Parser(tokens)
-        ast = parser.parse()
+def main():
+    while True:
+        try:
+            txt = input("calc > ")
+            lexer = Lexer(txt)
+            tokens = lexer.generate_tokens()
+            if len(sys.argv) >= 2 and sys.argv[1] == "--postfix":
+                print(postfix_shell(tokens))
+            else:
+                val = infix_shell(tokens)
+                if val == None:
+                    continue
 
-        if ast == None:
-            continue
+                print(val)
+        except Exception as e:
+            print(e)
 
-        interpreter = Interpreter(ast)
-        result = interpreter.eval()
-        print(result)
-    except Exception as e:
-        print(e)
+def postfix_shell(tokens):
+    return postfix_eval(tokens)
+
+def infix_shell(tokens):
+    parser = Parser(tokens)
+    ast = parser.parse()
+
+    if ast == None:
+        return None
+
+    interpreter = Interpreter(ast)
+    result = interpreter.eval()
+    return result
+
+
+if __name__ == "__main__":
+    main()
