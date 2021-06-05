5c40fc8f6511e865b5e6f58ab31e0bb6e18df6f5
Bryan Ngo <bryanngo@berkeley.edu>
2021-06-05 11:44:09 -0700

    add parsing stage

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from parser_ import Parser
 from lexer import Lexer
 
 while True:
     txt = input("calc > ")
     lexer = Lexer(txt)
     tokens = lexer.generate_tokens()
-    print(list(tokens))
+    parser = Parser(tokens)
+    ast = parser.parse()
+    print(ast)
